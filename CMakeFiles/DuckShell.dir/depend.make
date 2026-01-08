b83998e119f79f674722dc83512befc4dc6039ab
lucheshidi <lucheshidi@163.com>
2026-01-08 17:28:23 +0100

    Cleanup: remove build artifacts from git and fix gitignore

--- CMakeFiles/DuckShell.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for DuckShell.
-# This may be replaced when dependencies are built.
--- CMakeFiles/Makefile.cmake
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cdd2dde3f3dc6efeaa50503ee24fe4733b3d5e9d987d9624f0671c121bfb6034
-size 4779
